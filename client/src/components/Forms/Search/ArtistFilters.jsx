33f98936c03b99adec7e3087c39dcc1cb692cc6c
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-08-25 21:11:26 +0200

    filtrer les artistes

--- client/src/components/Forms/Search/ArtistFilters.jsx
+++ client/src/components/Forms/Search/ArtistFilters.jsx
@@ -1,28 +1,49 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 import { Box, HStack } from "@chakra-ui/react";
-import Input from "../../UI/Input";
-import GenreInput from "../../UI/GenreInput";
+import Input from "../../UI/InputComponent";
+import GenreInput from "../../UI/MultipleAutoCompleteInput";
 
-function ArtistFilters({ searchQuery, genre, onSearchChange, onGenreChange }) {
+function ArtistFilters({
+  nameFilter,
+  setNameFilter,
+  genreFilter,
+  setGenreFilter,
+}) {
+  const [displayGenre, setDisplayGenre] = useState("");
   return (
     <Box>
       <HStack spacing={4} mb={4}>
         <Input
-          value={searchQuery}
-          onChange={onSearchChange}
-          placeholder="Rechercher un artiste"
+          id="name"
+          label="Rechercher un artiste"
+          type="text"
+          placeholder="Motörhead"
+          value={nameFilter}
+          setValue={setNameFilter}
         />
-        <GenreInput value={genre} onChange={onGenreChange} />
+        <GenreInput
+          id="tag"
+          label="Genre"
+          placeholder="genre"
+          selectedItemIds={genreFilter}
+          setSelectedItemIds={setGenreFilter}
+          url="api/genreTag"
+          queryForTags=""
+          queryForInput=""
+          displayedValue={displayGenre}
+          setDisplayedValue={setDisplayGenre}
+        />
       </HStack>
     </Box>
   );
 }
 
 ArtistFilters.propTypes = {
-  searchQuery: PropTypes.string.isRequired,
-  genre: PropTypes.string.isRequired,
-  onSearchChange: PropTypes.func.isRequired,
-  onGenreChange: PropTypes.func.isRequired,
+  nameFilter: PropTypes.string.isRequired,
+  genreFilter: PropTypes.string.isRequired,
+  setNameFilter: PropTypes.func.isRequired,
+  setGenreFilter: PropTypes.func.isRequired,
 };
 
 export default ArtistFilters;
--- client/src/pages/explore/ListArtist.jsx
+++ client/src/pages/explore/ListArtist.jsx
@@ -1,12 +1,48 @@
+import { useState, useEffect } from "react";
 import { useLoaderData } from "react-router-dom";
 import { Box, Spinner, Text } from "@chakra-ui/react";
 import ArtistTable from "../../components/Detail/ArtistTable";
-// import SearchBlock from "../../components/Forms/Search/SearchBlock";
-// import ArtistFilters from "../../components/Forms/Search/ArtistFilters";
+import ArtistFilters from "../../components/Forms/Search/ArtistFilters";
+
+import connexion from "../../services/connexion";
 
 function ArtistList() {
   // Récupération des données via le loader
   const artists = useLoaderData();
+  const [nameFilter, setNameFilter] = useState("");
+  const [genreFilter, setGenreFilter] = useState([]);
+  const [filteredArtists, setFilteredArtists] = useState([]);
+
+  useEffect(() => {
+    const filterArtists = async () => {
+      let tempFilteredArtists = artists.filter((artist) =>
+        artist.name.toLowerCase().includes(nameFilter.toLowerCase())
+      );
+
+      if (genreFilter.length > 0) {
+        tempFilteredArtists = await Promise.all(
+          tempFilteredArtists.map(async (artist) => {
+            const response = await connexion.get(
+              `/api/genreTag?type=byArtist&artistId=${artist.id}`
+            );
+            const artistTags = response.data.map((tag) => tag.id);
+
+            const hasAllGenres = genreFilter.every((selectedTagId) =>
+              artistTags.includes(selectedTagId)
+            );
+
+            return hasAllGenres ? artist : null;
+          })
+        );
+        // Filtrer les résultats null (ceux qui ne correspondent pas au filtre)
+        tempFilteredArtists = tempFilteredArtists.filter(Boolean);
+      }
+
+      setFilteredArtists(tempFilteredArtists);
+    };
+
+    filterArtists();
+  }, [nameFilter, genreFilter, artists]);
 
   if (!artists) {
     return (
@@ -19,15 +55,13 @@ function ArtistList() {
 
   return (
     <Box>
-      {/* <SearchBlock filters={filters} onFilterRemove={handleFilterRemove}>
-        <ArtistFilters
-          searchQuery={filters.searchQuery}
-          genre={filters.genre}
-          onSearchChange={handleSearchChange}
-          onGenreChange={handleGenreChange}
-        />
-      </SearchBlock> */}
-      <ArtistTable artists={artists} />
+      <ArtistFilters
+        nameFilter={nameFilter}
+        setNameFilter={setNameFilter}
+        genreFilter={genreFilter}
+        setGenreFilter={setGenreFilter}
+      />
+      <ArtistTable artists={filteredArtists} />
     </Box>
   );
 }
