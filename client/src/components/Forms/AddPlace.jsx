3f1551b0dd5e77b12b618ec28248c51833215e2f
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-09-25 14:55:25 +0200

    autocompletion input

--- client/src/components/Forms/AddPlace.jsx
+++ client/src/components/Forms/AddPlace.jsx
@@ -1,5 +1,29 @@
+import {
+  GeoapifyGeocoderAutocomplete,
+  GeoapifyContext,
+} from "@geoapify/react-geocoder-autocomplete";
+
+import "@geoapify/geocoder-autocomplete/styles/minimal.css";
+import { useState } from "react";
+
 function AddPlace() {
-  return null;
+  const [inputValue, setInputValue] = useState("");
+  const handleSelect = ({ properties }) => {
+    setInputValue(properties.formatted);
+  };
+
+  return (
+    <GeoapifyContext apiKey={import.meta.env.VITE_GEOAPIFY_KEY}>
+      <GeoapifyGeocoderAutocomplete
+        placeholder="Enter address here"
+        value={inputValue}
+        type="amenity"
+        lang="fr"
+        limit={5}
+        placeSelect={handleSelect}
+      />
+    </GeoapifyContext>
+  );
 }
 
 export default AddPlace;
