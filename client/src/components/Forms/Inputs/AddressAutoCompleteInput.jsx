25cee763183f0b9b9e05928f98eeaf80dbc6c59d
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-09-25 18:03:03 +0200

    récupération des données en front

--- client/src/components/Forms/Inputs/AddressAutoCompleteInput.jsx
+++ client/src/components/Forms/Inputs/AddressAutoCompleteInput.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import {
   GeoapifyGeocoderAutocomplete,
   GeoapifyContext,
@@ -5,11 +6,22 @@ import {
 
 import "@geoapify/geocoder-autocomplete/styles/minimal.css";
 
-function AddressAutoCompleteInput(handleSelect) {
+function AddressAutoCompleteInput({ setLocationInfo }) {
+  const handleSelect = ({ properties }) => {
+    setLocationInfo({
+      address: properties.address_line1,
+      city: properties.city,
+      state: properties.state,
+      country: properties.country,
+      postal_code: properties.postcode,
+      latitude: properties.lat,
+      longitude: properties.lon,
+    });
+  };
   return (
     <GeoapifyContext apiKey={import.meta.env.VITE_GEOAPIFY_KEY}>
       <GeoapifyGeocoderAutocomplete
-        placeholder="Enter address here"
+        placeholder="Entrez une adresse"
         type="amenity"
         lang="fr"
         limit={5}
@@ -18,5 +30,9 @@ function AddressAutoCompleteInput(handleSelect) {
     </GeoapifyContext>
   );
 }
+
+AddressAutoCompleteInput.propTypes = {
+  setLocationInfo: PropTypes.func.isRequired,
+};
 
 export default AddressAutoCompleteInput;
